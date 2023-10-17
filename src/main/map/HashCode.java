c197b5e2e2b50f5852a65857ed652b9020c1b672
Dioneer <Gulkina_em@mail.ru>
2023-10-17 21:48:45 +0300

    hashCode

---
+++ src/main/map/HashCode.java
@@ -0,0 +1,54 @@
+package main.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashCode {
+    public static void main(String[] args) {
+        Student stt1 = new Student("Elena1", "Iknk", "3");
+        Student stt2 = new Student("Elena2", "Iknk", "3");
+        Student stt3 = new Student("Elena3", "Iknk", "3");
+        Map<Student, Double> st1= new HashMap<>();
+        st1.put(stt1, 7.5);
+        st1.put(stt2, 8.5);
+        st1.put(stt3, 9.5);
+        Student stt4 = new Student("Elena3", "Iknk", "3");
+        boolean bool = st1.containsKey(stt4);
+        System.out.println(bool);
+        System.out.println(st1);
+    }
+}
+final class Student{
+    final String name;
+    final String surname;
+    final String course;
+
+    public Student(String name, String surname, String course) {
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", course='" + course + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(surname, student.surname) && Objects.equals(course, student.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, course);
+    }
+}
---
+++ src/main/map/MapMy.java
@@ -0,0 +1,28 @@
+package main.map;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMy {
+    public static void main(String[] args) {
+        Map<Integer, String> map1 = new HashMap<>();
+        map1.put(12445,"Mirra Imn");
+        map1.put(32165,"Sira Jnh");
+        map1.put(89765,"ASdasd sdfs");
+        map1.put(45678,"Mike Popkin");
+        map1.put(456778,"Mike Popkin");
+        map1.put(null, null);
+        map1.remove(null);
+        map1.keySet();
+        System.out.println(map1.values());
+        System.out.println(map1.entrySet());
+        System.out.println(map1.containsValue("Mike Popkin"));
+        map1.putIfAbsent(456123,"Rik mjd");
+        System.out.println(map1.get(89765));
+        System.out.println(map1);
+        for(Map.Entry<Integer, String> item: map1.entrySet()){
+            System.out.println(item);
+        }
+    }
+}
