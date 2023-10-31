7662b593d4bd384133a9dd16956c18062e389c0a
Dioneer <gulkina_em@mail.ru>
2023-10-31 21:38:56 +0300

    string

---
+++ src/main/string/Abstr.java
@@ -0,0 +1,12 @@
+package main.string;
+
+public abstract class Abstr {
+    int i;
+    public Abstr(int i) {
+        this.i = i;
+    }
+
+    public void show() {
+        System.out.println(i);
+    }
+}
---
+++ src/main/string/Programm.java
@@ -0,0 +1,11 @@
+package main.string;
+
+public class Programm {
+    public static void main(String[] args) {
+//        Abstr ab = new Abstr(10){};
+//        ab.show();
+        String x = new String("Java");
+        System.out.println(x.charAt(1));
+        System.out.println(x.codePointAt(1));
+    }
+}
